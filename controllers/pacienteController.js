dc67732576d32e078132c2e312f134e592211565
alvcontrerass <alvarocontrerasilva@gmail.com>
2025-01-30 18:20:09 -0300

    implementado endpoints CRUD paciente

--- controllers/pacienteController.js
+++ controllers/pacienteController.js
@@ -22,17 +22,62 @@ export const obtenerPaciente = async (req, res) => {
 };
 
 export const obtenerPacienteEspecifico = async (req, res) => {
-    const { id } = req.params;
-    const paciente = await Paciente.findById(id);
-    if(paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
-        return res.json({msg: "Permiso denegado"});
-    }
+	const { id } = req.params;
+	const paciente = await Paciente.findById(id);
+
+	if (!paciente) {
+		res.status(404).json({ msg: "Paciente no encontrado" });
+	}
+
+	if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+		return res.json({ msg: "Permiso denegado" });
+	}
+
+	res.json(paciente);
 };
 
 export const actualizarPaciente = async (req, res) => {
+	const { id } = req.params;
+	const paciente = await Paciente.findById(id);
 
+	if (!paciente) {
+		return res.status(404).json({ msg: "Paciente no encontrado" });
+	}
+
+	if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+		return res.json({ msg: "Permiso denegado" });
+	}
+
+	paciente.nombre = req.body.nombre || paciente.nombre;
+	paciente.propietario = req.body.propietario || paciente.propietario;
+	paciente.email = req.body.email || paciente.email;
+	paciente.fecha = req.body.fecha || paciente.fecha;
+	paciente.sintomas = req.body.sintomas || paciente.sintomas;
+
+	try {
+		const pacienteActualizado = await paciente.save();
+		res.json(pacienteActualizado);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 export const eliminarPaciente = async (req, res) => {
+	const { id } = req.params;
+	const paciente = await Paciente.findById(id);
 
+	if (!paciente) {
+		return res.status(404).json({ msg: "Paciente no encontrado" });
+	}
+
+	if (paciente.veterinario._id.toString() !== req.veterinario._id.toString()) {
+		return res.json({ msg: "Permiso denegado" });
+	}
+
+	try {
+		await paciente.deleteOne();
+		res.json({ msg: "Paciente eliminado" });
+	} catch (error) {
+		console.log(error);
+	}
 };
